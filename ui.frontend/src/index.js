757c750951c26a5be1a68038468fb7a155f57d09
Ravi Singh <ravi.singh@publicissapient.com>
2024-11-05 15:19:03 +0530

    updated prompts

--- ui.frontend/src/index.js
+++ ui.frontend/src/index.js
@@ -5,13 +5,12 @@ import 'custom-event-polyfill';
 import { Constants, ModelManager } from '@adobe/aem-spa-page-model-manager';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import { render } from 'react-dom';
+import { render, } from 'react-dom';
 import { Router } from 'react-router-dom';
 import App from './App';
 import LocalDevModelClient from './LocalDevModelClient';
 import './components/import-components';
 import './index.css';
-
 const modelManagerOptions = {};
 if(process.env.REACT_APP_PROXY_ENABLED) {
     modelManagerOptions.modelClient = new LocalDevModelClient(process.env.REACT_APP_API_HOST);
